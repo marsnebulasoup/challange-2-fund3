b883e21774908a81d576849f8c11a20fc600d5fa
marsnebulasoup <52974834+marsnebulasoup@users.noreply.github.com>
2022-11-21 13:15:33 -0600

    Implemented hashtable

--- hash.h
+++ hash.h
@@ -7,6 +7,39 @@ A implementation of a hash table.
 #ifndef HASH_H
 #define HASH_H
 
+#include "data.h"
+#include "string"
+#include <iostream>
+#include <iomanip>
 
+using std::cout;
+using std::endl;
+using std::left;
+using std::setw;
+using std::string;
+
+#define SIZE 15
+
+class HashTable
+{
+private:
+  int count;
+  Node *table[SIZE];
+  int hash(int);
+
+  bool addNode(Node **, int, string *);
+  bool deleteNode(Node **, int);
+  bool getNode(Node *, Data *, int);
+  void printList(Node *);
+
+public:
+  HashTable();
+
+  bool insertEntry(int, string *);
+  bool removeEntry(int);
+  bool getData(int, Data *);
+  int getCount();
+  void printTable();
+};
 
 #endif
